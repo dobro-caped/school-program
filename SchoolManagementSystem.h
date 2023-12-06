0786d2619bc64d806736c32d211e732bf9b68f28
Lora Sher <152856649+dobro-caped@users.noreply.github.com>
2023-12-06 10:36:57 +0600

    Create SchoolManagementSystem.h

---
+++ SchoolManagementSystem.h
@@ -0,0 +1,15 @@
+// SchoolManagementSystem.h
+#pragma once
+#include "Student.h"
+#include "Teacher.h"
+#include "Course.h"
+
+class SchoolManagementSystem {
+public:
+    SchoolManagementSystem();
+    void run();
+
+private:
+    // Add necessary member variables and functions
+    // Example: vector<Student> students;
+};
