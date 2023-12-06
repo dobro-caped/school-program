ff1012cb26c21ae95866f7f4540c6d6ce9c86dc5
Lora Sher <152856649+dobro-caped@users.noreply.github.com>
2023-12-06 10:38:12 +0600

    Create SchoolManagementSystem.cpp

---
+++ SchoolManagementSystem.cpp
@@ -0,0 +1,13 @@
+// SchoolManagementSystem.cpp
+#include "SchoolManagementSystem.h"
+#include <iostream>
+
+SchoolManagementSystem::SchoolManagementSystem() {
+    // Initialize member variables
+}
+
+void SchoolManagementSystem::run() {
+    // Implement the main functionality of the school management system
+    // Example: Display menu, handle user input, etc.
+    std::cout << "School Management System is running..." << std::endl;
+}
